335c534a4623955a543f0652178cd8b69b7a0849
RodLopezDev <rodrigolopezdev@gmail.com>
2021-08-14 10:42:39 -0500

    feat: Initial deploy

---
+++ docs/static/js/main.a56c9cb7.chunk.js
@@ -0,0 +1,2 @@
+(this.webpackJsonpenara=this.webpackJsonpenara||[]).push([[0],{9:function(n,e,t){"use strict";t.r(e);var c=t(1),r=t.n(c),i=t(3),o=t.n(i),a=t(0);var s=function(){return Object(a.jsx)("div",{children:"Hola mundo"})},u=function(n){n&&n instanceof Function&&t.e(3).then(t.bind(null,10)).then((function(e){var t=e.getCLS,c=e.getFID,r=e.getFCP,i=e.getLCP,o=e.getTTFB;t(n),c(n),r(n),i(n),o(n)}))};o.a.render(Object(a.jsx)(r.a.StrictMode,{children:Object(a.jsx)(s,{})}),document.getElementById("root")),u()}},[[9,1,2]]]);
+//# sourceMappingURL=main.a56c9cb7.chunk.js.map
